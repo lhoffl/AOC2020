294fb50ce549ac2b1120abd2309eaf63e0f4dea2
Matthew Hoffman <matt@lhoffl.com>
2020-12-01 23:40:44 -0500

    Solution for part two added

--- day1/day1.rb
+++ day1/day1.rb
@@ -5,9 +5,24 @@ File.foreach('input.txt') { |line| input_set.add(line.chomp!.to_i) }
 
 input_set.each do |number| 
     difference = (number - 2020).abs()
-    if(input_set.include? difference)
-        puts "#{number} + #{difference} = 2020"
+    if input_set.include? difference
         answer = number * difference
-        puts "Answer is #{answer}"
+        puts "Answer to part one is #{answer}"
+        break
+    end
+end
+
+input_set.each do |number| 
+    difference = (number - 2020).abs()
+    input_set.each do |another_number|
+        next if number == another_number or another_number == difference or number == 0
+        another_difference = (another_number - difference).abs()
+        
+        if input_set.include? another_difference
+            answer_as_number = number + another_number + another_difference
+            answer = number * another_number * another_difference
+            puts "Answer to part two is #{answer}"
+            return
+        end
     end
 end

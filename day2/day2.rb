16abbb2143d0c196fea1722cf38af8cc4df0c30d
Matthew Hoffman <matt@lhoffl.com>
2020-12-02 11:49:25 -0500

    get both answers in one loop

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,15 +1,14 @@
-input_file = File.read('input.txt')
-
 valid_password_count1 = 0
 valid_password_count2 = 0
-input_file.each_line do |line|
-    tokens = line.split
-    numbers = tokens[0].split('-')
-    range = numbers[0].to_i .. numbers[1].to_i
-    
-    first_position = numbers[0].to_i - 1
-    second_position = numbers[1].to_i - 1
 
+File.foreach('input.txt') do |line|
+    tokens = line.split
+    
+    numbers = tokens[0].split('-')
+    first_number = numbers[0].to_i
+    second_number = numbers[1].to_i
+    range = first_number .. second_number
+    
     required_character = tokens[1].gsub(/:/, '')
     password = tokens[2].split('')
 
@@ -20,12 +19,13 @@ input_file.each_line do |line|
     end
     
     count = 0
-    count += 1 if (password[first_position] == required_character)
-    count += 1 if (password[second_position] == required_character)
+    count += 1 if (password[first_number - 1] == required_character)
+    count += 1 if (password[second_number - 1] == required_character)
     
     if count == 1
         valid_password_count2 += 1
     end
 end
+
 puts "Answer for part one is #{valid_password_count1}"
 puts "Answer for part two is #{valid_password_count2}"

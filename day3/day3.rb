f4676cccfba4e30df7463ce2917175871d968194
Matthew Hoffman <matt@lhoffl.com>
2020-12-03 19:44:26 -0500

    added day3 part1

--- day3/day3.rb
+++ day3/day3.rb
@@ -5,22 +5,15 @@ right_move_amount = 3
 
 number_of_trees = 0
 next_index = 0
-count = 0
-output = ''
 
 input_file.each_with_index do |character, index|
 
-    draw_character = '.'
-
     if index == 0
-        draw_character = '0'
         number_of_trees += 1 if character == '#'
         next_index = line_length + right_move_amount
 
     elsif index == next_index 
-        draw_character = '0'
         number_of_trees += 1 if character == '#'
-        draw_character = 'X' if character == '#'
     
         next_index = index + right_move_amount
         remainder = index % line_length
@@ -29,10 +22,6 @@ input_file.each_with_index do |character, index|
             next_index += line_length
         end
     end
-    
-    draw_character = '#' if character == '#' and draw_character != 'X'
-    output += "\n" if index % line_length == 0
-    output += draw_character
 end
 
 puts number_of_trees
